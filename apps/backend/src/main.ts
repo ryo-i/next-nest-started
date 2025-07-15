f66640b40c235162df98cd4fcc377cc7d06c7720
ryo-i <iidazz0109@gmail.com>
2025-07-15 23:49:20 +0900

    簡易的なAPIを作成

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: require('path').resolve(__dirname, '../../../.env') });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors({
+    origin: process.env.FRONTEND_ORIGIN,
+    credentials: true,
+  });
+  await app.listen(process.env.BACKEND_PORT ?? 3001);
 }
 bootstrap();
