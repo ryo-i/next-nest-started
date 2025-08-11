88ca0cd97a4100d945c49ac4da9bd2e36e60bdb6
ryo-i <iidazz0109@gmail.com>
2025-08-11 19:39:31 +0900

    formatを実行

--- apps/backend/src/prisma/prisma.module.ts
+++ apps/backend/src/prisma/prisma.module.ts
@@ -4,6 +4,6 @@ import { PrismaService } from './prisma.service';
 @Global() // グローバルモジュールとして登録
 @Module({
   providers: [PrismaService], // PrismaService を提供
-  exports: [PrismaService],   // 他のモジュールで利用可能にする
+  exports: [PrismaService], // 他のモジュールで利用可能にする
 })
 export class PrismaModule {}
